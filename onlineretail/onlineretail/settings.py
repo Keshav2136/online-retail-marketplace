0c28e708dd0cb926501d04b792933b8ad7250d9f
Keshav Sharma <keshav.simple.123@gmail.com>
2021-08-25 14:37:05 +0530

    Getting ready for deploying on Heroku

--- onlineretail/onlineretail/settings.py
+++ onlineretail/onlineretail/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bulma',
     'home',
 ]
 
@@ -124,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static")
+]
